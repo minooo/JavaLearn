d3c986936056a46a9237b83d55d73e5dded23026
minooo <552594996@qq.com>
2017-12-20 09:03:03 +0800

    s

--- ClassObject/src/com/minooo/animal/Cat.java
+++ ClassObject/src/com/minooo/animal/Cat.java
@@ -7,8 +7,16 @@ public class Cat {
     double weight;
     String species;
 
+    public Cat(String name, int age, double weight, String species) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.species = species;
+    }
+
     // 成员方法
     public void run() {
+        this.eat();
         System.out.println("小猫快跑");
     }
 
--- ClassObject/src/com/minooo/animal/CatTest.java
+++ ClassObject/src/com/minooo/animal/CatTest.java
@@ -2,16 +2,9 @@ package com.minooo.animal;
 
 public class CatTest {
     public static void main(String[] args) {
-        Cat one = new Cat();
+        Cat one = new Cat("花花", 3, 1000, "中华猫");
 
         // 测试
-        one.eat();
-        one.run();
-        one.name = "七七";
-        one.age = 2;
-        one.weight = 1000;
-        one.species = "中华田园猫";
         System.out.println("昵称：" + one.name);
-        one.run(one.name);
     }
 }
