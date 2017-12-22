7dfd7fc78da9b5e0c70ccd10dc27b7557c98ea36
minooo <552594996@qq.com>
2017-12-22 09:15:55 +0800

    s

--- ClassObject/src/com/minooo/animal/Cat.java
+++ ClassObject/src/com/minooo/animal/Cat.java
@@ -1,34 +1,81 @@
+// package com.minooo.animal;
+//public class Cat {
+//    // 成员属性
+//    String name;
+//    int age;
+//    double weight;
+//    String species;
+//
+//    public Cat() {
+//
+//    }
+//
+//    public Cat(String name, int age, double weight, String species) {
+//        this.name = name;
+//        this.age = age;
+//        this.weight = weight;
+//        this.species = species;
+//    }
+//
+//    // 成员方法
+//    public void run() {
+//        this.eat();
+//        System.out.println("小猫快跑");
+//    }
+//
+//    public void run1(){
+//        System.out.println(name + "快跑");
+//    }
+//
+//    public void eat() {
+//        System.out.println("小猫吃鱼");
+//    }
+//}
+
 package com.minooo.animal;
 
 public class Cat {
     // 成员属性
-    String name;
-    int age;
-    double weight;
-    String species;
+    private String name;
+    private int age;
+    private double weight;
+    private String species;
 
-    public Cat() {
-
+    public Cat(int month) {
+        //this.age = month;
+        this.setAge(month);
     }
 
-    public Cat(String name, int age, double weight, String species) {
+    // 创建 get/set 方法
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return "我是一只叫做：" + this.name + "的宠物猫";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
         this.weight = weight;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
         this.species = species;
-    }
-
-    // 成员方法
-    public void run() {
-        this.eat();
-        System.out.println("小猫快跑");
-    }
-
-    public void run1(){
-        System.out.println(name + "快跑");
-    }
-
-    public void eat() {
-        System.out.println("小猫吃鱼");
     }
 }
--- ClassObject/src/com/minooo/animal/CatTest.java
+++ ClassObject/src/com/minooo/animal/CatTest.java
@@ -1,13 +1,26 @@
+//package com.minooo.animal;
+//
+//public class CatTest {
+//    public static void main(String[] args) {
+//        Cat one = new Cat("花花", 3, 1000, "中华猫");
+//        Cat two = new Cat();
+//        two.name = "haha";
+//        two.run1();
+//        one.run1();
+//        // 测试
+//        // System.out.println("昵称：" + one.name);
+//    }
+//}
+
+
 package com.minooo.animal;
 
 public class CatTest {
     public static void main(String[] args) {
-        Cat one = new Cat("花花", 3, 1000, "中华猫");
-        Cat two = new Cat();
-        two.name = "haha";
-        two.run1();
-        one.run1();
-        // 测试
-        // System.out.println("昵称：" + one.name);
+        Cat one = new Cat(3);
+        System.out.println(one.getAge());
+
+//        one.setName("花花");
+//        System.out.println(one.getName());
     }
 }
